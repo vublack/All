fe09f002de6ea2800a9cdcd4c4f6314a9030bf46
vasyl.chornyi <vublack@gmail.com>
2019-05-06 10:47:06 +0300

    parameter chooseGKD

--- src/test/java/com/bars/AllCreditTest.java
+++ src/test/java/com/bars/AllCreditTest.java
@@ -76,12 +76,12 @@ public class AllCreditTest extends BaseLoginTest {
 //        switchWindowOrFrame.switchToOldWindow(newKdUoWindow);
 //        newCreditPage.productOfCredit2();
 //        newCreditPage.filterInput(ConfigProperties.getTestProperty("productuo"));
-/*
+
         if( base.equals("OBMMFOT")|| base.equals("OBMMFOT1"))
         {
             newCreditPage.chooseGKD();
         }
-*/
+
         //Вкладка Дані про погашення
         String firstPaymentDate = newCreditPage.getConclusionDate();
         newCreditPage.firstPaymentDate(firstPaymentDate);
